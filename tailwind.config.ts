3219f0ea5b9e2fdc1ad54bea8c397d44a9a826e0
kimhwanjin917 <hwanjin2@naver.com>
2026-07-12 02:52:25 +0900

    Revert "히어로 전면 교체: 밤의 호텔 스카이라인 비주얼"
    This reverts commit 66415d282a66e2e9a2bf55948d0e62a7693b156e.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,16 +11,6 @@ const config: Config = {
       fontFamily: {
         sans: ['Pretendard Variable', 'Pretendard', ...defaultTheme.fontFamily.sans],
       },
-      keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
-        },
-      },
-      animation: {
-        float: 'float 6s ease-in-out infinite',
-        'float-slow': 'float 8s ease-in-out infinite',
-      },
     },
   },
   plugins: [],
